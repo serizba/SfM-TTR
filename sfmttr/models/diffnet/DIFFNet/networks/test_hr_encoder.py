c76c579cb8f4a12f9272efe7e89e3c7c432732b2
Sergio Izquierdo <serizba@gmail.com>
2022-11-15 17:42:10 +0100

    DIFFNet: fix test_hr_encoder.py

--- sfmttr/models/diffnet/DIFFNet/networks/test_hr_encoder.py
+++ sfmttr/models/diffnet/DIFFNet/networks/test_hr_encoder.py
@@ -3,7 +3,8 @@ import torch
 import logging
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
 
 import matplotlib.pyplot as plt
 logger = logging.getLogger('hrnet_backbone')
